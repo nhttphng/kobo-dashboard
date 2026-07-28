e7dd2d49670d444f48338fa1d703546f10e670b4
nhi <soidontforgetyesterday@gmail.com>
2026-07-27 22:03:33 -0600

    fix dates

--- calendar_processor.py
+++ calendar_processor.py
@@ -150,7 +150,7 @@ class CalendarProcessor:
                 'description': str(event_component.get('description', '')),
                 'location': str(event_component.get('location', '')),
                 'calendarSource': calendar_name,
-                "isAllDay": not isinstance(event.dtstart, datetime) # True if it was a 'date' object
+                "isAllDay": not isinstance(dtstart, datetime) # True if it was a 'date' object
             }
             
         except Exception as e:
