eec8bb001372743c3c8932b84dca1cf985279faa
nhi <soidontforgetyesterday@gmail.com>
2026-07-27 22:16:23 -0600

    try another

--- calendar_processor.py
+++ calendar_processor.py
@@ -150,7 +150,7 @@ class CalendarProcessor:
                 'description': str(event_component.get('description', '')),
                 'location': str(event_component.get('location', '')),
                 'calendarSource': calendar_name,
-                "isAllDay": not isinstance(dtstart.dt, datetime)
+                "isAllDay": not hasattr(dtstart.dt, 'hour')
             }
             
         except Exception as e:
